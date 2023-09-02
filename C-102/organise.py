fc22599bce9067a2ef97c9372cd4c9a852206fd2
Amrit <116807282+AmritSaha123@users.noreply.github.com>
2023-09-02 14:25:33 +0800

    Add files via upload

---
+++ C-102/organise.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+from_dir = "/Users/amritsaha/Desktop/C-102"
+to_dir = "/Users/amritsaha/Desktop/destination"
+listOfFiles = os.listdir(from_dir)
+print(listOfFiles)
+for f in listOfFiles:
+    name,ext = os.path.splitext(f)
+    print(name)
+    print(ext)
+    if ext =="":
+        continue
+    if ext in ['.gif','.png','.jpg','.jfif','.jpeg']:
+        path1 = from_dir+"/"+f
+        path2 = to_dir+"/"+"img_files"
+        path3 = to_dir+"/"+"img_files"+"/"+f
+        if os.path.exists(path2):
+            print("moving "+f)
+            shutil.move(path1,path3)
+        
+        else:
+            os.makedirs(path2)
+            print("moving "+f)
+            shutil.move(path1,path3)
